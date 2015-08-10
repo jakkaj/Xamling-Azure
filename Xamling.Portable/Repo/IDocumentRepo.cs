1bfcaf8a72c20cd90b876583992e36098c90ac90
Jordan Knight <jakkaj@gmail.com>
2015-08-10 17:23:06 +1000

    Randome changes

---
+++ Xamling.Portable/Repo/IDocumentRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamling.Azure.Portable.Contract.Cache;
+using XamlingCore.Portable.Contract.Entities;
+
+namespace Xamling.Azure.Portable.Repo
+{
+    public class DocumentRepo
+    {
+        private readonly IDocumentEntityCache _documentCache;
+        private readonly IRedisEntityCache _redisCache;
+        private List<IEntityCache> _caches;
+
+        public DocumentRepo(IDocumentEntityCache documentCache, IRedisEntityCache redisCache)
+        {
+            _documentCache = documentCache;
+            _redisCache = redisCache;
+        }
+    }
+}
